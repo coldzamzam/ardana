c5f956f6441183ff3412600a8c925d85d93a41a4
ginangela <angelsoen1998@gmail.com>
2025-12-09 13:41:36 +0700

    gajadi wok

--- resources/js/components/ui/dialog.tsx
+++ resources/js/components/ui/dialog.tsx
@@ -1,5 +1,7 @@
 import * as React from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
+import { XIcon } from "lucide-react"
+
 import { cn } from "@/lib/utils"
 
 function Dialog({
@@ -59,7 +61,10 @@ function DialogContent({
         {...props}
       >
         {children}
-        {/* Tombol X dihapus dari sini */}
+        <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4">
+          <XIcon />
+          <span className="sr-only">Close</span>
+        </DialogPrimitive.Close>
       </DialogPrimitive.Content>
     </DialogPortal>
   )
